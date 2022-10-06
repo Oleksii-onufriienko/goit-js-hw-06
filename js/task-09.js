f34d543dbf174d3ead9ac60ca3e654998b16315e
Oleksii Onufriienko <o.alexey1202@gmail.com>
2022-10-06 23:03:43 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refButton = document.querySelector(".widget button.change-color");
+const refBody = document.querySelector("body");
+const refSpan = document.querySelector(".widget span.color");
+
+refButton.addEventListener("click", () => {
+  const tmpColor = getRandomHexColor();
+  refBody.style.backgroundColor = tmpColor;
+  refSpan.textContent = tmpColor;
+});
