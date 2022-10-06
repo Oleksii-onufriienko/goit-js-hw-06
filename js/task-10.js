5b4000b340df464aeca1062b09bc3d67f62b60bc
Oleksii Onufriienko <o.alexey1202@gmail.com>
2022-10-06 23:59:37 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const ref = {
+  Input: document.querySelector("input[type=number]"),
+  Create: document.querySelector("button[data-create]"),
+  Destroy: document.querySelector("button[data-destroy]"),
+};
+
+console.log(ref);
+
+ref.Create.addEventListener("click", () => {
+  createBoxes(ref.Input.value);
+});
+
+function createBoxes(amount) {
+  console.log(amount);
+}
