c60a8c9939405f90f0d894e24bfee63abc403767
Oleksii Onufriienko <o.alexey1202@gmail.com>
2022-10-06 22:37:16 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const refForm = document.querySelector(".login-form");
 
+refForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Все поля должны быть заполнены!");
+    return;
+  }
+  const dataForm = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(dataForm);
+  refForm.reset();
+});
