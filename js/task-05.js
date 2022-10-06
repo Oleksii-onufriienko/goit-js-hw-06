4eaa17b4d8483c57e2e49b50f48f810c510c0fc6
Oleksii Onufriienko <o.alexey1202@gmail.com>
2022-10-06 18:57:41 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const refInput = document.querySelector("#name-input");
+const refOutput = document.querySelector("#name-output");
+
+console.log(refInput);
+console.log(refOutput);
+
+refInput.addEventListener("input", (event) => {
+  refOutput.textContent = event.currentTarget.value;
+  console.log(event);
+});
